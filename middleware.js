817fd06df204ada7fbc34f9036cac07d8001d59b
mouislambd <tanhaislammou097@gmail.com>
2026-05-03 14:08:50 -0700

    update sections newly build and dbuging

--- middleware.js
+++ middleware.js
@@ -1,9 +1,39 @@
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//     const session =
+//         request.cookies.get("better-auth.session_token") ||
+//         request.cookies.get("__Secure-better-auth.session_token");
+
+//     const { pathname } = request.nextUrl;
+
+//     const privateRoutes = ["/my-profile", "/tile"];
+
+//     const isPrivate = privateRoutes.some((route) =>
+//         pathname.startsWith(route)
+//     );
+
+//     if (isPrivate && !session) {
+//         return NextResponse.redirect(new URL("/login", request.url));
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/my-profile/:path*", "/tile/:path*"],
+// };
+
+
+
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
     const session =
         request.cookies.get("better-auth.session_token") ||
-        request.cookies.get("__Secure-better-auth.session_token");
+        request.cookies.get("__Secure-better-auth.session_token") ||
+        request.cookies.get("better-auth.session_token.0") ||
+        request.cookies.get("__Secure-better-auth.session_token.0");
 
     const { pathname } = request.nextUrl;
 
