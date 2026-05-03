93b8fed0deeb274203102ceb7f3970fc9dd6f401
mouislambd <tanhaislammou097@gmail.com>
2026-05-03 09:56:22 -0700

    tile file ready for site

--- app/tile/[id]/page.jsx
+++ app/tile/[id]/page.jsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
+
 export default function TileDetailPage() {
     const { id } = useParams();
     const [tile, setTile] = useState(null);
